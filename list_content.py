bc8999fcff7f7c849abc0265e27ee53e9bd0b550
Jcowell <Jcowwell@gmail.com>
2021-03-15 09:28:09 -0400

    + args in list_content.py, added skeleton to ipynb

--- list_content.py
+++ list_content.py
@@ -1,6 +1,8 @@
 # PURP: - TO HANLDE INITAL LOGGING AND ARGS LOGIC. 
 import logging
 import argparse
+from drive_dump import dump_drives, dump_drive
+from info_logger import log_drive_info
 from constants import D, DRV, L, FLD, F, FIL, T, TYP, LOG_FORMAT, LOG_DATE_FORMAT
 
 
@@ -24,9 +26,6 @@ parser.add_argument(TYP, help='list additional info of type of single file')
 logging.debug("Args Added")
 
 args = parser.parse_args(args=['-d', 'D'])
-
-from drive_dump import dump_drives, dump_drive
-from info_logger import log_drive_info
 
 # MARK:- Begin Args Parameters Handeling
 logging.debug("Beginning Args Handeling...")
@@ -52,3 +51,27 @@ if args.drv:
     else:
         drive_name, num_of_files_and_dirs, storage = drive
         log_drive_info(drive_name, num_of_files_and_dirs, storage)
+
+# MARK:- -l args
+if args.l:
+    logging.debug('-d argument was passed')
+
+# MARK:- --fld args
+if args.drv:
+    logging.debug('--fld argument was passed')
+
+# MARK:- -f args
+if args.f:
+    logging.debug('-f argument was passed')
+
+# MARK:- --fil args
+if args.fil:
+    logging.debug('--fil argument was passed')
+
+# MARK:- -t args
+if args.t:
+    logging.debug('-t argument was passed')
+
+# MARK:- -typ args
+if args.typ:
+    logging.debug('--typ argument was passed')
